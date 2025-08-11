bccc755b7d1e9067eed2c6d0c06218553808d2c8
mmh11 <huim13a@gmail.com>
2025-08-11 17:23:42 +1000

    Fixed lambda function parking bay query for continuous period with same rule

--- lambda/parking_bay_query/lambda_function.py
+++ lambda/parking_bay_query/lambda_function.py
@@ -1,6 +1,5 @@
 import pymysql
 import json
-from collections import defaultdict
 
 # Configuration
 endpoint = 'parking-db-prod.cziu20u0uide.ap-southeast-2.rds.amazonaws.com'
@@ -11,8 +10,6 @@ database_name = 'parking_db'
 def lambda_handler(event, context):
     # Connection
     connection = pymysql.connect(host=endpoint, user=username, passwd=password, db=database_name)
-
-    # Query
     cursor = connection.cursor()
     query = """
         SELECT 
@@ -27,27 +24,25 @@ def lambda_handler(event, context):
             r.end_time,
             r.rule
         FROM staging_parking_sensors ps
-        LEFT JOIN staging_restrictions r
+        LEFT JOIN parking_restrictions_clean_v r
             ON CAST(ps.Zone_Number AS UNSIGNED) = CAST(r.zone_id AS UNSIGNED)
     """
     cursor.execute(query)
-
     result = cursor.fetchall()
 
-    # Group by KerbsideID
     grouped = {}
     for row in result:
         kerb_id = row[4]
         restriction = {
             'Days': row[6],
-            'Start_Time': row[7],
-            'End_Time': row[8],
+            'Start_Time': str(row[7]) if row[7] is not None else None,
+            'End_Time': str(row[8]) if row[8] is not None else None,
             'Rule': row[9]
         }
         if kerb_id not in grouped:
             grouped[kerb_id] = {
-                'Lastupdated': row[0],
-                'Status_Timestamp': row[1],
+                'Lastupdated': str(row[0]) if row[0] else None,
+                'Status_Timestamp': str(row[1]) if row[1] else None,
                 'Zone_Number': row[2],
                 'Status_Description': row[3],
                 'KerbsideID': row[4],
